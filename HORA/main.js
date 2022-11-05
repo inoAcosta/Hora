a038982a674dc0add211f7cb48770ad304479c1d
inoAcosta <1380357@senati.pe>
2022-11-04 21:33:10 -0500

    Creando hora con HTML, CSS Y JS

---
+++ HORA/main.js
@@ -0,0 +1,20 @@
+function inicioHora(){
+    var today = new Date();
+    var hr = today.getHours();
+    var min = today.getMinutes();
+    var sec = today.getSeconds();
+    
+    ap = (hr < 12) ? "<span> A:M  </span>: <span> P:M </span>";
+    hr = (hr == 0) ? hr -12 :hr;
+
+    hr = checkTime(hr);
+    min = checkTime(min);
+    sec = checkTime(sec);
+
+    document.getElementById("Hora").innerHTML = hr;
+    document.getElementById("Minutos").innerHTML = min;
+    document.getElementById("Segundos").innerHTML = sec;
+    document.getElementById("ampm").innerHTML = ap;
+
+
+}
